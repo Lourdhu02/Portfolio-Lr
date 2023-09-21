a58f9354ec3827ffa79fa4694d64455f4fa9f450
Lourdhu Raju <109408033+Lourdhu02@users.noreply.github.com>
2023-09-21 16:22:35 +0530

    Update rightbox.jsx

--- src/components/rightbox/rightbox.jsx
+++ src/components/rightbox/rightbox.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import "./rightbox.css"
 import download from "../../assets/download.png"
-import { motion, useInView, useAnimation, easeInOut, AnimatePresence } from "framer-motion";
+
 
 
  
 
 function RightBox() {
   return <div>
-    <motion.div className="animation">
+    <div className="animation">
       <img src={download}  alt="logo" className="animated_logo" />
-    </motion.div>
+    </div>
   </div>
 }
 export default RightBox
